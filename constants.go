8d0f244fabc387a66cd836d6981be43432163e91
Shirley Zhang <shirley.zhang@hotmail.com.au>
2016-12-28 09:33:54 +1100

    Roll dice function works now

---
+++ constants.go
@@ -0,0 +1,3 @@
+package main
+
+const CHANNEL_SERVER_CHATTER = "259926279820804097"
--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"strings"
+	"time"
 
+	"github.com/ShirleyZ/godice"
 	"github.com/bwmarrin/discordgo"
 )
+
+// const CHANNEL_SERVER_CHATTER = "259926279820804097"
 
 // Variables used for command line parameters
 var (
@@ -34,14 +39,15 @@ func main() {
 	if err != nil {
 		fmt.Println("error obtaining account details,", err)
 	}
-	fmt.Println("=== Received my user details. They are: ");
-	fmt.Println(u);
+	fmt.Println("=== Received my user details. They are: ")
+	fmt.Println(u)
 
 	// Store the account ID for later use.
 	BotID = u.ID
 
 	// Register messageCreate as a callback for the messageCreate events.
 	dg.AddHandler(messageCreate)
+	dg.AddHandler(messageDelete)
 
 	// Open the websocket and begin listening.
 	err = dg.Open()
@@ -60,6 +66,10 @@ func main() {
 // message is created on any channel that the autenticated bot has access to.
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
+	// Print message to stdout.
+	fmt.Printf("%20s %20s %20s > %s\n", m.ChannelID, time.Now().Format(time.Stamp), m.Author.Username, m.Content)
+	// fmt.Print(m)
+
 	// Ignore all messages created by the bot itself
 	if m.Author.ID == BotID {
 		return
@@ -67,15 +77,76 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	// If the message is "ping" reply with "Pong!"
 	if m.Content == "ping" {
-		_, _ = s.ChannelMessageSend(m.ChannelID, "Pong!")
+		fmt.Print("Executing cmd: ping")
+		_, err := s.ChannelMessageSend(m.ChannelID, "Pongg!")
+		if err != nil {
+			fmt.Println("ping: channelmsgsend error")
+			log.Fatal(err)
+		}
 	}
 
 	// If the message is "pong" reply with "Ping!"
 	if m.Content == "pong" {
-		_, _ = s.ChannelMessageSend(m.ChannelID, "Ping!")
+		fmt.Println("Executing cmd: pong")
+		_, err := s.ChannelMessageSend(m.ChannelID, "Ping!")
+		if err != nil {
+			fmt.Print("pong: channelmsgsend error")
+			log.Fatal(err)
+		}
+	}
+
+	// Show information about the message sent
+	if m.Content == "!thism" {
+		fmt.Println("Executing cmd: !thism")
+		channel, err := s.Channel(m.ChannelID)
+		if err != nil {
+			fmt.Printf("!thism cannot find channel with that id")
+			log.Fatal(err)
+		}
+		msg := "Sent: " + m.Timestamp + "\n"
+		msg += "Channel: " + channel.Name + "(" + m.ChannelID + ")\n"
+		msg += "isPrivate: " + fmt.Sprintf("%t", channel.IsPrivate) + "\n"
+		if channel.IsPrivate == true {
+			msg += "Recipient: " + channel.Recipient.Username
+		}
+
+		_, err = s.ChannelMessageSend(m.ChannelID, msg)
+		if err != nil {
+			fmt.Println("!thism: Channel msg send unsuccessful")
+			log.Fatal(err)
+			// return
+		}
+	}
+
+	// If the message is "!test" send the message to server-chatter
+	if m.Content == "!test" {
+		// dice.Roll("1d5")
+	}
+
+	// Dice command
+	fmt.Printf("\n%s\n", m.Content)
+	if strings.HasPrefix(m.Content, "!!roll") {
+		fmt.Print("Rolling")
+		result, err := dice.Roll(m.Content)
+		if err == nil {
+			_, err = s.ChannelMessageSend(m.ChannelID, result)
+			if err != nil {
+				fmt.Println("!!roll: Channel msg send unsuccessful")
+				log.Fatal(err)
+			}
+		}
 	}
 
 	if strings.Contains(m.Content, "well done") {
-		_, _ = s.ChannelMessageSend(m.ChannelID, "Thank you!")
+		_, err := s.ChannelMessageSend(m.ChannelID, "Thank you!")
+		if err != nil {
+			fmt.Println("well done: Channel msg send unsuccessful")
+			log.Fatal(err)
+		}
 	}
+
+}
+
+func messageDelete(s *discordgo.Session, m *discordgo.MessageDelete) {
+	fmt.Printf("A message has been deleted")
 }
